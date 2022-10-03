e16a0070cf880bead37302227c3f24e15e158fca
DaniloProfita <danoprofi11@gmail.com>
2022-10-03 17:04:13 -0300

    Primer commit en la rama JavaScript
    Primer commit

---
+++ JavaScript/leccion2/02-01-TiposDatos.js
@@ -0,0 +1,14 @@
+//Tipos de datos en JS
+/*Otro tipo de comentarios, pero de multiple lineas*/
+var nombre = 'Arial';//Tipo Str
+console.log(nombre);
+
+var numero = 3000; //Tipo numero
+console.log(numero);
+
+var objeto = {
+    nombre: "Ariel",
+    apellido: "Bentacud",
+    telefono: "123456"
+};
+console.log(objeto);
