92b538b153f3d023d09439f9b05dcb4c02169865
DaniloProfita <danoprofi11@gmail.com>
2022-10-04 21:16:51 -0300

    Clase 4 - Tipos de datos
    Reutilizar variables
    Repaso - Creacion del archivo index.html
    Tipos de datos booleanos, funcion y symbol
    Definir un tipo clase

--- JavaScript/leccion2/02-01-TiposDatos.js
+++ JavaScript/leccion2/02-01-TiposDatos.js
@@ -1,7 +1,12 @@
 //Tipos de datos en JS
 /*Otro tipo de comentarios, pero de multiple lineas*/
 var nombre = 'Arial';//Tipo Str
-console.log(nombre);
+console.log(typeof nombre);
+nombre = 7;
+console.log(typeof nombre);
+nombre = 12.3;
+console.log(typeof nombre);
+
 
 var numero = 3000; //Tipo numero
 console.log(numero);
@@ -12,3 +17,26 @@ var objeto = {
     telefono: "123456"
 };
 console.log(objeto);
+
+//Tipo de dato boolean
+var bandera = true;
+console.log(bandera);
+
+
+//Tipo de dato funcion
+function miFuncion(){}
+console.log(typeof miFuncion)
+
+
+//Tipo de dato symbol
+var simbolo = Symbol("Mi símbolo");
+console.log(typeof simbolo);
+
+//Tipo de dato calse
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(typeof Persona);
