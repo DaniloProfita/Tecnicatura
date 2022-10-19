06678a6a127719cf463a0b0eca3e4a069dd7fb3f
DaniloProfita <danoprofi11@gmail.com>
2022-10-19 17:15:31 -0300

    Leccion02 de JavaScript y similitudes de sintaxis con Java
    Leccion02 de JavaScript y similitudes de sintaxis con Java

--- JavaScript/leccion2/02-01-TiposDatos.js
+++ JavaScript/leccion2/02-01-TiposDatos.js
@@ -40,3 +40,23 @@ class Persona{
     }
 }
 console.log(typeof Persona);
+
+//Tipo de dato undefined
+var x;
+console.log(x);
+
+x = undefined;
+console.log(typeof x);
+
+//null: significa ausencia de valor
+var y = null;//null no es un tipo de dato, pero su origen es tipo object
+console.log(typeof y);
+
+//Tipo de dato array y Empty String
+var autos = ['Citroen', 'Audi', 'BMW', 'Ford']
+console.log(autos);
+console.log(typeof autos);
+
+var z = '';
+console.log(z);//Cadena vacia
+console.log(typeof z);//Cadena vacia
