fb068d1c749af3a8c335f70c1ac2c6754560057a
DaniloProfita <danoprofi11@gmail.com>
2022-10-26 21:43:51 -0300

    Clase 7 JavaScript
    Tipos de datos, Operadores y Ejercicios

--- JavaScript/leccion02/02-01-ConcatenacionDeCadenas.js
+++ JavaScript/leccion02/02-01-ConcatenacionDeCadenas.js
@@ -4,12 +4,25 @@ var nombreCompleto = nombre+' '+apellido;//Primera concatenacion
 console.log(nombreCompleto);
 var nombreCompleto2 = 'Ariel'+' '+'Bentacud';//Segunda concatenacion
 console.log(nombreCompleto2);
+
 var juntos = nombre + 219;//Lee de izq a der siguiendo la cadena lee el numero como str
 console.log(juntos);
+
 juntos = nombre + 78 + 17;//Aqui se puede diferenciar a traves de los parentesis
 console.log(juntos);
+
 juntos = 78 + 17 + nombre;
 console.log(juntos);
 
 nombre += apellido;//Tercera concatenacion usando el operador simplificado
 console.log(nombre);
+
+let x, y; //Se pueden crear varias variables dentro de una misma linea
+x = 17, y = 21; //Se puede hacer asignacion de varias variables dentro de la misma linea
+let z = x + y; //Se asigna el valor de la operacion
+console.log(z);
+
+let _1num = 31;//No utilizar numero para iniciar el nombre de una variable
+let rompiendo = 'rompe'; //No utilizar palabras reserbadas para variables
+console.log(_1num);
+console.log(rompiendo);
---
+++ JavaScript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,15 @@
+//Ejercicio para encontrar numeros pares
+let numero = 7;
+if(numero % 2 == 0){
+    console.log("Es un numero par");
+} else {
+    console.log("Es un numero impar");
+}
+
+//Ejercicio: es mayor de edad
+let edad = 10;
+if(edad > 18){
+    console.log("Es mayor de edad");
+} else {
+    console.log("Es menor de edad");
+}
