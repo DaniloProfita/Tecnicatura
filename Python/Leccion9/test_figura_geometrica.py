e4569166ea2a0ecd861c09f67332b644f74d08fe
DaniloProfita <danoprofi11@gmail.com>
2022-11-10 16:19:15 -0300

    Clase 13 de Python
    Hablamos de validaciones, Metodos encapsulados, validaciones en setters, clases Abstractas y el comienzo de contexto estatico

--- Python/Leccion9/test_figura_geometrica.py
+++ Python/Leccion9/test_figura_geometrica.py
@@ -1,16 +1,24 @@
 from Cuadrado import Cuadrado
+from FiguraGeometrica import FiguraGeometrica
 from Rectangulo import Rectangulo
 
-cuadrado1 = Cuadrado(5, 'Azul')
-print(cuadrado1.ancho)
-print(cuadrado1.alto)
+print("Creacion de objeto clase Cuadrado".center(50, '_'))
+cuadrado1 = Cuadrado(8, 'Azul')
+cuadrado1.alto = 7
+#cuadrado1.ancho = 7
+#print(cuadrado1.ancho)
+#print(cuadrado1.alto)
 print(f'Calculo del Area del cuadrado: {cuadrado1.calcular_area()}')
 
 #MRO = Method Resolution Order
-print(Cuadrado.mro())
+#print(Cuadrado.mro())
 
 print(cuadrado1)
-
-rectangulo1 = Rectangulo(5, 10, 'Rojo')
+print("Creacion de objeto clase Cuadrado".center(50, '_'))
+rectangulo1 = Rectangulo(3, 9, 'Rojo')
+rectangulo1.ancho = 8
 print(f'Calculo del area del rectangulo: {rectangulo1.calcular_area()}')
 print(rectangulo1)
+
+#figura1 = FiguraGeometrica() No se puede instanciar, es abstracta
+print(Cuadrado.mro())
